9d91d63d76be7d77ac9e50662a2f41f53264cba8
Ender Calderón <177250779+Yohebyth@users.noreply.github.com>
2024-11-04 01:11:03 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -5,7 +5,35 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = () => {
+  const cardValue = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
+  const cardSuit = [
+    { symbol: "♥", name: "hearts" },
+    { symbol: "♦", name: "diamonds" },
+    { symbol: "♣", name: "clubs" },
+    { symbol: "♠", name: "spades" }
+  ];
+  const ramdomFuntion = lengthParam => {
+    return Math.floor(Math.random() * lengthParam);
+  };
+
+  function randomCard() {
+    let randomCardValue = values[ramdomFuntion(cardValue.length)];
+    let randomCardSuit = suits[ramdomFuntion(cardSuit.length)];
+
+    document.getElementById("cardValue").innerText = randomCardValue;
+    document.getElementById("topSuit").innerText = randomCardSuit.symbol;
+    document.getElementById("bottomSuit").innerText = randomCardSuit.symbol;
+
+    document.getElementById(
+      "topSuit"
+    ).className = `suit suit-top ${randomCardSuit.name}`;
+    document.getElementById(
+      "bottomSuit"
+    ).className = `suit suit-bottom ${randomCardSuit.name}`;
+    document.getElementById(
+      "cardValue"
+    ).className = `value ${randomCardSuit.name}`;
+  }
+  randomCard();
 };
